a96e8b4e96463ad471ac8ed7cd7e385d3fc1e0e6
vitorbss12 <vitor_bss10@hotmail.com>
2022-02-03 13:56:40 -0500

    Atualizando exercicios de fixação JavaScript - Introdução e lógica de programação

---
+++ Bloco-04-Introducao-a-JavaScript-e-Logica-de-Programacao/Dia-01-JavaScript-Primeiros-passos/Fixando/Fixando-5.js
@@ -0,0 +1,7 @@
+let weekDay = "domingo";
+
+if ( weekDay === "segunda" || weekDay === "terca" || weekDay === "quarta" || weekDay === "quinta" || weekDay === "sexta") {
+    console.log("dia de trybe");
+} else if ( weekDay === "sabado" || weekDay === "domingo" ) {
+    console.log("descanso");
+};
