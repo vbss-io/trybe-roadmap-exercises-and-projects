e9c596001c44091de5c3aed9cce42bda813b3a57
vitorbss12 <vitor_bss10@hotmail.com>
2022-02-10 12:01:46 -0500

    Uptade files JavaScript Primeiros Passos

--- Bloco-04-Introducao-a-JavaScript-e-Logica-de-Programacao/Dia-01-JavaScript-Primeiros-passos/Exercicio-9.js
+++
@@ -1,9 +0,0 @@
-let a = 2;
-let b = 2;
-let c = 2;
-
-if (a%2 != 0 || b%2 != 0 || c%2 != 0) {
-    console.log(true);
-} else {
-    console.log(false);
-};
---
+++ Bloco-04-Introducao-a-JavaScript-e-Logica-de-Programacao/Dia-01-JavaScript-Primeiros-passos/Maior-3-numeros.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 30;
+let c = 40;
+
+
+if (a > b && a > c) {
+  console.log("Maior (a): ", a);
+} else if (b > a && b > c) {
+  console.log("Maior (b): ", b);
+} else if (c > a && c > b) {
+  console.log("Maior (c): ", c);
+} else {
+  console.log("Dois ou mais numeros iguais.");
+}
