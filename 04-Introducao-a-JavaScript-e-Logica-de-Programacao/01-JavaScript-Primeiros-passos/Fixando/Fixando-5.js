0d34bd370cd3ec0d51d812d2c6510d62ea2d6de0
vitorbss12 <vitor_bss10@hotmail.com>
2022-02-10 12:22:34 -0500

    Files update JavaScript Primeiros Passos

--- 04-Introducao-a-JavaScript-e-Logica-de-Programacao/01-JavaScript-Primeiros-passos/Fixando/Fixando-5.js
+++ 04-Introducao-a-JavaScript-e-Logica-de-Programacao/01-JavaScript-Primeiros-passos/Fixando/Fixando-5.js
@@ -1,7 +1,7 @@
 let weekDay = "domingo";
 
-if ( weekDay === "segunda" || weekDay === "terca" || weekDay === "quarta" || weekDay === "quinta" || weekDay === "sexta") {
-    console.log("dia de trybe");
-} else if ( weekDay === "sabado" || weekDay === "domingo" ) {
-    console.log("descanso");
+if (weekDay === "segunda" || weekDay === "terca" || weekDay === "quarta" || weekDay === "quinta" || weekDay === "sexta") {
+  console.log("dia de trybe");
+} else if (weekDay === "sabado" || weekDay === "domingo") {
+  console.log("descanso");
 };
