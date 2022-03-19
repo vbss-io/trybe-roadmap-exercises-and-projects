cb27c1fa58e7d0a57fc93bc5af623f11a627c5ef
vitorbss12 <vitor_bss10@hotmail.com>
2022-03-19 17:10:14 -0500

    Exercicios Bloco 8.2

--- 08-Higher-Order-Functions-JavaScript-ES6/02-Higher-Order-Functions-forEach-find-some-every-sort/Fixando/script.js
+++ 08-Higher-Order-Functions-JavaScript-ES6/02-Higher-Order-Functions-forEach-find-some-every-sort/Fixando/script.js
@@ -50,6 +50,56 @@ function findMusic(id) {
   console.log(musicas.find((musicid) => musicas.id === musicid));
 }
 
-console.log(findMusic('31031685'))
+// console.log(findMusic('31031685'))
 
-console.log(musicas[0].id);
+// console.log(musicas[0].id);
+
+// --------------------------------------------------------------------------------------
+// Escreva uma função que dado um array de nomes e um nome retorne true se ele estiver contido e caso contrário, retorne false , use some: 
+
+const namesSome = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  return arr.some((element) => element === name);
+
+}
+
+// console.log(hasName(namesSome, 'Ana'))
+
+// --------------------------------------------------------------------------------------
+// Escreva uma função que dado um array de pessoas e uma idade mínima retorne true se todas tiverem a idade maior ou igual a mínima e caso contrário false , use every;
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 19 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 15 },
+  { name: 'Bruna', age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  return arr.every((element) => element.age >= minimumAge);
+}
+
+// console.log(verifyAges(people, 18));
+
+// --------------------------------------------------------------------------------------
+// Utilize a sort para ordenar o array pela idade das pessoas em ordem crescente .
+
+const peopleSort = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+peopleSort.sort((a, b) => a.age - b.age);
+
+// console.log(peopleSort);
+
+// Modifique o sort do exercício anterior para que ordene o array pela idade das pessoas em ordem decrescente.
+
+peopleSort.sort((a, b) => b.age - a.age);
+
+console.log(peopleSort);
