cc02482ebb1f72662681c8f11148146ca2110fc6
vitorbss12 <vitor_bss10@hotmail.com>
2022-09-19 20:34:08 -0300

    feat: Exercícios back-end 27.3 - POO - Polimorfismo

---
+++ 03-Desenvolvimento-Back-End/09-POO-e-SOLID/03-Polimosfismo/README.md
@@ -0,0 +1,8 @@
+## Hard Skills Desenvolvidas
+
+### Polimorfismo
+
+- [X] _Descrever, como pilar da POO, o polimorfismo: múltiplas formas de se implementar um método descrito em uma interface_
+- [X] _Descrever Classe Abstrata como uma classe que não pode ser instanciada, devendo ser usada apenas por suas Classes Herdeiras_
+- [X] _Definir Métodos Abstratos, ou Funções Abstratas_
+- [X] _Implementas Métodos Estáticos para Classes em TypeScript_
---
+++ 03-Desenvolvimento-Back-End/09-POO-e-SOLID/03-Polimosfismo/conteudo/src/Exemplos.ts
@@ -0,0 +1,97 @@
+class Animal {
+  constructor(public name: string) { }
+  move() { console.log(`${this.name} está se movendo.`); }
+}
+class Bird extends Animal {
+  move() { console.log(`${this.name} está voando.`); }
+}
+class Mammal extends Animal {
+  move() { console.log(`${this.name} está andando.`); }
+}
+
+const a = new Animal('Tubarão');
+const b = new Bird('Papagaio');
+const m = new Mammal('Tatu');
+
+const myMove = (animal: Animal) => {
+  animal.move();
+}
+myMove(a);
+myMove(b);
+myMove(m);
+
+/*
+Saída:
+Tubarão está se movendo.
+Papagaio está voando.
+Tatu está andando.
+*/
+
+// Uso do super:
+
+// class Animal {
+//   constructor(public name: string) { }
+//   move() { console.log(`${this.name} está se movendo.`); }
+// }
+// class Bird extends Animal {
+//   move() {
+//     super.move();
+//     console.log(`${this.name} está voando.`);
+//   }
+// }
+// class Mammal extends Animal {
+//   move() { console.log(`${this.name} está andando.`); }
+// }
+
+// const a = new Animal('Tubarão');
+// const b = new Bird('Papagaio');
+// const m = new Mammal('Tatu');
+
+// const myMove = (animal: Animal) => {
+//   animal.move();
+// }
+// myMove(a);
+// myMove(b);
+// myMove(m);
+
+/*
+Saída:
+Tubarão está se movendo.
+Papagaio está se movendo.
+Papagaio está voando.
+Tatu está andando.
+*/
+
+// Classes Abstratas e Métodos Abstratos:
+
+abstract class Animal2 {
+  constructor(public name: string) { }
+  abstract move(): void
+}
+class Bird2 extends Animal2 {
+  move() { console.log(`${this.name} está voando.`); }
+}
+class Mammal2 extends Animal2 {
+  move() { console.log(`${this.name} está andando.`); }
+}
+class Fish2 extends Animal2 {
+  move() { console.log(`${this.name} está nadando.`); }
+}
+const a2 = new Fish2('Tubarão');
+const b2 = new Bird2('Papagaio');
+const m2 = new Mammal('Tatu');
+const myMove2 = (animal: Animal2) => {
+  animal.move();
+}
+myMove2(a2);
+myMove2(b2);
+myMove2(m2);
+/*
+Saída:
+Tubarão está nadando.
+Papagaio está voando.
+Tatu está andando.
+*/
+
+// Métodos estáticos:
+// Um método estático nada mais é do que uma função que não precisa acessar nenhum atributo do objeto.
